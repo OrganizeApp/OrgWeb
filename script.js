57835ba00e26c6e2226c2be754161e31553051de
Salman <salmanfakhri2@gmail.com>
2016-02-13 21:16:40 -0500

    dashboard

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var ultData = new Firebase("https://cheultimate.firebaseio.com/");
 
+//AUTH STUFF
 
-
-function login() {
+function loginWithGoogle() {
 	alert("work?");
 	ultData.authWithOAuthPopup("google", function(error, authData) {
   		if (error) {
@@ -17,11 +17,31 @@ function login() {
 	
 };
 
+function loginWithFacebook() {
+	openWin();
+};
+
 function openWin(){
 	window.open("dashboard.html", "_parent");
 }
 
-function nameUpdate() {
-	console.log(authData);
+// after the page redirects
+function loadFacebookAuthData() {
+	ultData.authWithOAuthPopup("facebook", function(error, authData) {
+  		if (error) {
+    		console.log("Login Failed!", error);
+  		} else {
+    		console.log("Authenticated successfully with payload:", authData);
+    		var greeting = document.getElementById('welcome');
+			var profilePic = document.getElementById('profilePic');
+
+			greeting.innerHTML = authData.facebook.displayName;
+			profilePic.src = authData.facebook.profileImageURL;
+  		}
+	});
+
+
 }
 	
+
+
